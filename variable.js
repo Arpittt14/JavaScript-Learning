154d4fa51b8c8932cc5203041cab4e1f22ab8691
Arpittt14 <ap0970375@gmail.com>
2026-06-25 22:24:13 +0530

    Let,const & var

--- variable.js
+++ variable.js
@@ -18,3 +18,21 @@ console.log(y);
  * Variable names cannot start with a number.
  * Reserved words (like JavaScript keywords) cannot be used as variable names.
  */
+
+// Let, Const & Var 
+
+/** 
+ * Let, Const, and Var are used to declare variables in JavaScript.
+ * Let and Const were introduced in ES6 (2015) to provide more control over variable scope.
+ * Var is the older way of declaring variables and has function scope.
+ * let allows you to declare block-scoped variables, while const is used to declare variables that cannot be reassigned.
+ */
+
+let price = 25; // Block-scoped variable
+let fullname = "Kaushal"; // Block-scoped variable 
+
+console.log(price);
+console.log(fullname);
+
+const pi = 3.14; // Constant variable
+console.log(pi);
