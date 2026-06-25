db6e5972a53f430f8fad851091c01476ba612927
Arpittt14 <ap0970375@gmail.com>
2026-06-25 22:48:40 +0530

    Practice 1 added

---
+++ Practice/practice1.js
@@ -0,0 +1,11 @@
+// Create a const object called "Product" to store information shown in the image below.
+// Use the following keys: name, price, category, inStock, and tags.
+
+const product = {
+    name : "Ball Pen",
+    Price : "150",
+    Category : "Stationary",
+    inStock : true,
+    tags : ["Pen", "Stationary", "Ball Pen"],
+    ratings : 4.5 
+}
