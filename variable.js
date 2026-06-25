53f3c0cd321be6e825c667bc93f262ba3addf4f6
Arpittt14 <ap0970375@gmail.com>
2026-06-25 22:06:16 +0530

    Variables are added

---
+++ variable.js
@@ -0,0 +1,20 @@
+name = "Arpit Patel";
+age = 20;
+x = null; 
+isFollow = false;
+isNotFollow = true;
+y = undefined;
+
+console.log(x);
+console.log(name);
+console.log(age);
+console.log(isFollow);
+console.log(isNotFollow);
+console.log(y);
+
+/** 
+ * Variable names are case sensitive "a" and "A" are different variables.
+ * Only letters, numbers, underscores, and dollar signs can be used in variable names.
+ * Variable names cannot start with a number.
+ * Reserved words (like JavaScript keywords) cannot be used as variable names.
+ */
