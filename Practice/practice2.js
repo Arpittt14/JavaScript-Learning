a74ce825fa5a65508446e01ed6c687362a00e395
Arpittt14 <ap0970375@gmail.com>
2026-06-25 22:58:22 +0530

    .

---
+++ Practice/practice2.js
@@ -0,0 +1,11 @@
+// create a const object called "Profile" to store information shown in the image below.
+
+const profile = {
+    name : "Arpitt_patell",
+    followers : 400,
+    following : 300,
+    bio : "Live, Laugh, Love",
+    isFollow : true
+}
+
+console.log(profile);
