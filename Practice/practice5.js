9e90ad32256184ff4e35630876b55b44456b83e6
Arpittt14 <ap0970375@gmail.com>
2026-06-26 22:13:10 +0530

    Practice 5 is added

---
+++ Practice/practice5.js
@@ -0,0 +1,15 @@
+// Print all even numbers from 0 to 100 
+
+// for loop 
+
+for(let i = 0 ; i <=100 ; i = i + 2){
+    console.log(i);
+}  
+
+// while loop
+
+let x = 0;
+while(x== 100){
+    console.log(x);
+    x = x + 2;
+} 
