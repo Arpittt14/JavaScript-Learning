717ea0b95420def0033af17ab9ed31d9b4030173
Arpittt14 <ap0970375@gmail.com>
2026-06-26 21:30:26 +0530

    Loops are added

---
+++ Loops/for-loop.js
@@ -0,0 +1,10 @@
+// For Loop it is used to repeat a block of code a certain number of times. It is often used when the number of iterations is known beforehand.
+
+// Syntax of for loop
+// for(initialization; condition; increment/decrement){
+//     // code to be executed
+// } 
+
+for(let i = 0; i<=5; i++){
+    console.log("Arpit Patel"); 
+}
