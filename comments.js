848a7ca14f6631b1a8073ba0558f158bdd067fec
Arpittt14 <ap0970375@gmail.com>
2026-06-26 12:07:35 +0530

    Comments are added

---
+++ comments.js
@@ -0,0 +1,10 @@
+// This is a Single line  comment 
+/* This is a Multi-line comment
+   It can span multiple lines */ 
+
+
+// ==========================================
+
+/* We use comments to explain our code,
+   make it more readable, and to leave notes for ourselves or other developers. 
+   Comments are ignored by the JavaScript engine during execution. */ 
