876e1f8a02d3a7a62f15aeb5193c227fc019e660
Arpittt14 <ap0970375@gmail.com>
2026-07-16 20:25:36 +0530

    forEach loop is added

---
+++ Loops/forEach-loop.js
@@ -0,0 +1,9 @@
+// forEach loop is used to iterate over an array and execute a provided function once for each array element.
+
+const fruits = ['apple', 'banana', 'cherry']; 
+
+fruits.forEach(function(fruit){
+    console.log(fruit); 
+
+}) 
+
